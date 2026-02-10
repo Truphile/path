e32c5e76c6429722242efe44ea80f1b4d0b132e8
Truphile <salidmonreal@gmail.com>
2026-02-10 16:11:30 +0100

    file and path commit

--- path/src/Example.java
+++ path/src/Example.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -5,8 +7,23 @@ public class Example {
     public static void main(String[] args) {
         String fileLocation = "/home/scripted/IdeaProjects";
         Path path = Path.of(fileLocation);
-
         System.out.println(path.getFileSystem());
+
+        String fileLoca = "/home/scripted/Documents/NewJavaWorks/path/src";
+        String filename = "user.json";
+        Path file = Path.of(fileLoca,filename);
+        try {
+            Files.createFile(file);
+        }catch (IOException e){
+            e.printStackTrace();
+
+        }
+
+
+
+
     }
 }
+
+
 
