7507d833d91062edcad76224d70234d72f4021e1
Truphile <salidmonreal@gmail.com>
2026-02-11 16:07:03 +0100

    Bytebase Streem commit

--- path/src/streams/input/cloudDataReader.java
+++ path/src/streams/input/cloudDataReader.java
@@ -1,18 +1,36 @@
 package streams.input;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 
 public class cloudDataReader {
 
-    public static String readDataFrom(String cloudLocation) {
-        URI uri = URI.create(cloudLocation);
-        try (InputStream inputStream = uri.toURL().openStream()) {
+    public static void main(String[] args) {
+        final String file = "/home/scripted/Documents/NewJavaWorks/path/src/streams/output/post.json";
+        String url = "https://dummyjson.com/products";
+        String data = readDataFrom(url);
 
-        }catch (IOException e){
+        try (FileWriter writer = new FileWriter(file);) {
+            writer.write(data);
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
+
+
+    public static String readDataFrom(String cloudLocation) {
+        URI uri = URI.create(cloudLocation);
+        try (InputStream inputStream = uri.toURL().openStream()) {
+            byte[] data = inputStream.readAllBytes();
+            return new String(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+    }
+
 }
